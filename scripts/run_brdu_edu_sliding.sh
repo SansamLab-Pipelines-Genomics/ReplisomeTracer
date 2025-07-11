f0903b2172ba5e6435d2e5edaf2e4c30a9b7b406
Krishanu-Dhar <krishanuxaverian2001@gmail.com>
2025-07-11 11:01:21 -0500

    Update run_brdu_edu_sliding.sh

--- scripts/run_brdu_edu_sliding.sh
+++ scripts/run_brdu_edu_sliding.sh
@@ -5,22 +5,22 @@
 #SBATCH --time=02:00:00
 #SBATCH --partition=serial
 
-# This shell script is to run 'brdu_edu_sliding_windows_from_bam.py' on a HPC cluster
+# This shell script is to run 'brdu_edu_sliding_windows_from_bam.py' on an HPC cluster
 
 # === Loading modules ===
 module purge
-module load slurm python pandas numpy matplotlib
+module load slurm python pandas matplotlib
 
 # === Paths and parameters ===
-FILTERED_BED="/s/sansam-lab/CS_20250702_Rep/ReplisomeTracer/DNAscent_Snakemake/results/forkSense/Aux_1/final_filtered_trial.bed"
-BRDU_PREFIX="/s/sansam-lab/CS_20250702_Rep/ReplisomeTracer/analyze_bams/Aux_trial/BrdU_"
-EDU_PREFIX="/s/sansam-lab/CS_20250702_Rep/ReplisomeTracer/analyze_bams/Aux_trial/EdU_"
-BAM_FILE="/s/sansam-lab/CS_20250702_Rep/ReplisomeTracer/DNAscent_Snakemake/results/detect_combined/Aux_1.bam"
+FILTERED_BED="/s/sansam-lab/path/to/filtered_bedfile"
+BRDU_PREFIX="/s/sansam-lab/path/to/directory/to_store/All_bedgraphs/BrdU_"
+EDU_PREFIX="/s/sansam-lab/path/to/directory/to_store/All_bedgraphs/EdU_"   #Keep the directories common for both the EdU and BrdU bedgraphs
+BAM_FILE="/s/sansam-lab/path/to/directory/containing/the/corresponding_bamfile/"
 WINDOW_SIZE=100
 STEP_SIZE=10
 
-# === Run the script ===
-python /s/sansam-lab/Try2_RepNano/RepNano/scripts/brdu_edu_sliding_windows_from_bam.py \
+# === Run the Python script ===
+python /s/sansam-lab/path/to/Python_script/scripts/brdu_edu_sliding_windows_from_bam.py \
   --bam "$BAM_FILE" \
   --brdu_output "$BRDU_PREFIX" \
   --edu_output "$EDU_PREFIX" \
