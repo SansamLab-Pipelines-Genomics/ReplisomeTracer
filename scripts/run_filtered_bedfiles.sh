6b4a72d0d23b079d2e5fc5eca7ff0bdc75abb13a
Krishanu-Dhar <krishanuxaverian2001@gmail.com>
2025-07-11 11:41:11 -0500

    Update run_filtered_bedfiles.sh

--- scripts/run_filtered_bedfiles.sh
+++ scripts/run_filtered_bedfiles.sh
@@ -10,9 +10,9 @@ LEFT_FORKS="/s/sansam-lab/CS_20250702_Rep/ReplisomeTracer/DNAscent_Snakemake/res
 RIGHT_FORKS="/s/sansam-lab/CS_20250702_Rep/ReplisomeTracer/DNAscent_Snakemake/results/forkSense/Aux_1/rightForks_DNAscent_forkSense.bed"
 ORIGIN="/s/sansam-lab/CS_20250702_Rep/ReplisomeTracer/DNAscent_Snakemake/results/forkSense/Aux_1/origins_DNAscent_forkSense.bed"
 TERMINATION="/s/sansam-lab/CS_20250702_Rep/ReplisomeTracer/DNAscent_Snakemake/results/forkSense/Aux_1/terminations_DNAscent_forkSense.bed"
-BED_OUTPUT="/s/sansam-lab/CS_20250702_Rep/ReplisomeTracer/DNAscent_Snakemake/results/forkSense/Aux_1/final_filtered_trial.bed"
+BED_OUTPUT="/s/sansam-lab/CS_20250702_Rep/ReplisomeTracer/DNAscent_Snakemake/results/forkSense/Aux_1/final_filtered.bed"
 
-python /s/sansam-lab/Try2_RepNano/RepNano/scripts/filtered_bedfiles.py \
+python /s/sansam-lab/path/to/scripts/filtered_bedfiles.py \
   --left "$LEFT_FORKS" \
   --right "$RIGHT_FORKS" \
   --origin "$ORIGIN" \
